b1afc6a3aebd115ef1e2265ac91c84f26ea987b2
BuffaloRob <peckham.rob@gmail.com>
2018-11-19 14:14:02 -0500

    Index: replaced catsReducer with rootReducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import catsReducer from './reducers/cats_reducer'
 
-const store = createStore(catsReducer, applyMiddleware(thunk))
+const rootReducer = combineReducers({
+    cat: catsReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const app = (
     <Provider store={store}>
