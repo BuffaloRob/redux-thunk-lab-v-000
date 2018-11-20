3c41e3c6bb5cbdef087deca0f7adc595a32f6281
BuffaloRob <peckham.rob@gmail.com>
2018-11-20 09:46:34 -0500

    + code to use redux devtools

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import catsReducer from './reducers/cats_reducer'
 
 const rootReducer = combineReducers({
     cat: catsReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 const app = (
     <Provider store={store}>
